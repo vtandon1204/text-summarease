320d16db4d54fdcb9f03886972d77b63186d0e15
Vaibhav Tandon <vasu.tan2017@gmail.com>
2024-10-30 00:26:29 +0530

    model evaluation done

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from textSummarizer.pipeline.stage01_data_ingestion import DataIngestionTraining
 from textSummarizer.pipeline.stage02_data_validation import DataValidationTrainingPipeline  
 from textSummarizer.pipeline.stage03_data_transformation import DataTransformationTrainingPipeline
 from textSummarizer.pipeline.stage04_model_trainer import ModelTrainerTrainingPipeline
+from textSummarizer.pipeline.stage05_model_evaluation import ModelEvaluationTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -52,19 +53,17 @@ try:
 except Exception as e:
         logger.exception(e)
         raise e
+    
 
-
-
-
-# STAGE_NAME = "Model Evaluation stage"
-# try: 
-#    logger.info(f"*******************")
-#    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-#    model_evaluation = ModelEvaluationTrainingPipeline()
-#    model_evaluation.main()
-#    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
-# except Exception as e:
-#         logger.exception(e)
-#         raise e
+STAGE_NAME = "Model Evaluation stage"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_evaluation = ModelEvaluationTrainingPipeline()
+   model_evaluation.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
 
 
--- test.py
+++ test.py
@@ -1,11 +1,14 @@
-import os
+# import os
 
-path = 'D:/technical/AI-ML/ml_projects/text-summarizer-project/artifacts/data_transformation/samsum_dataset/test/data-00000-of-00001.arrow'
+# path = 'D:/technical/AI-ML/ml_projects/text-summarizer-project/artifacts/data_transformation/samsum_dataset/test/data-00000-of-00001.arrow'
 
-directory = os.path.dirname(path)
-if not os.path.exists(directory):
-    print(f"Directory does not exist: {directory}")
-else:
-    print("Directory exists.")
-if len(path) > 260:
-    print("Path exceeds maximum length.")
+# directory = os.path.dirname(path)
+# if not os.path.exists(directory):
+#     print(f"Directory does not exist: {directory}")
+# else:
+#     print("Directory exists.")
+# if len(path) > 260:
+#     print("Path exceeds maximum length.")
+from pathlib import Path
+config_filepath = Path('config/config.yaml')
+print(f"Config file path: {config_filepath}")
