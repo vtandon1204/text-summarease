4ed034127e997f8bdbf27f18434d1598c28e0935
Vaibhav Tandon <vasu.tan2017@gmail.com>
2025-03-07 22:07:00 +0530

    commit

--- app/app_flask.py
+++ app/app_flask.py
@@ -31,5 +31,8 @@ def summarize():
 
     return jsonify({'summary': summary})
 
-if __name__ == '__main__':
-    app.run(host="127.0.0.1", port=5000, debug=True)
+# if __name__ == '__main__':
+#     app.run(host="127.0.0.1", port=5000, debug=True)
+
+def handler(event, context):
+    return app(event, context)
